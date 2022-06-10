ff3fecd0a1e12de698f3260dd259885b4363c621
priyam26 <priyamahali2611@gmail.com>
2022-06-10 21:32:54 +0530

    Revert "update"
    This reverts commit e347e4417a92aa03e8a4e7b7f658420e3eb042a2.

--- src/Heading.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Heading()
-{
-    return <h1>This is priya</h1>;
-}
-export default Heading;
--- src/Para.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Para()
-{
-    return<p>This is my Ordered list</p>
-}
-export default Para;
